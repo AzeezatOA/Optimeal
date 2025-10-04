5a7d95e947304065f107d3c719fcdce8e722e33c
Rachel Yiu <rachelyiu@MacBook-Air-6.local>
2025-10-04 16:44:58 -0500

    Fixed home page

--- optimeal-app/src/app/home/home.page.ts
+++ optimeal-app/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, CommonModule],
   standalone: true
 })
 export class HomePage {
